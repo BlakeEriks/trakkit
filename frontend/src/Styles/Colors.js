84dc6f4a8d6a71a6954db8a5e4fe976121416862
Blake Eriks <eblake98@vt.edu>
2021-12-18 14:26:21 -0800

    Migrate to tailwind

--- frontend/src/Styles/Colors.js
+++
@@ -1,7 +0,0 @@
-export default {
-    dark: 'rgb(1, 22, 39)',
-    light: 'rgb(244, 244, 244)',
-    light2: 'rgba(211, 211, 211, 0.502)',
-    highlight: 'rgb(65, 234, 212)',
-    accent: 'rgb(209, 0, 84)'
-}
