a7b7f06b3a0984be82f308d4d7f5c85d7fda33bf
Blake Eriks <eblake98@vt.edu>
2021-12-18 17:54:27 -0800

    Add react query

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App';
+import './index.css';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      keepPreviousData: true,
+      staleTime: Infinity,
+      refetchOnWindowFocus: false,
+    }
+  },
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>,
   document.getElementById('root')
 );
